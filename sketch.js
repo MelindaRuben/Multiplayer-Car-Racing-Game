e6cc8cb049b41da02b1716c113a3c0af3dcb333e
Melinda Ruben <meli.ruben@gmail.com>
2021-01-23 14:35:45 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var backgroundImage, database, canvas;
+var gameState = 0;
+var playerCount;
+var form, player,game;
+
+
+function setup(){
+  canvas = createCanvas(400, 400);
+  database = firebase.database();
+  console.log(database);
+
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){}
